49a729ec3c378591b9848e3d3baf949289aa5009
Harold Ritter <harold.ritter@gmail.com>
2022-09-05 17:22:40 +0200

    Adds the GSIncType enumerator

---
+++ source/IncType.enum.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const readonly = require("./utils/readonly");
+
+var GSIncType =
+{
+	// Gets the name of the position from the include type:
+	// "before", "after", "later", "insert" or "insertOnce".
+	// The default include type is "before".
+	toString: function(incType)
+	{
+		return incType === GSIncType.LATER ? "later" :
+					incType === GSIncType.AFTER ? "after" :
+						incType === GSIncType.INSERT ? "insert" :
+							incType === GSIncType.INSERT_AND_CACHE ? "insertOnce" :
+								"before";
+	},
+
+	// Gets the include type from the argument
+	get: function(incType)
+	{
+		if(	typeof(incType) !== "number" ||
+			// Does not use => and <= because it
+			// does not work if the indices change
+			incType != GSIncType.AFTER &&
+			incType != GSIncType.LATER &&
+			incType != GSIncType.INSERT &&
+			incType != GSIncType.INSERT_AND_CACHE)
+		{
+			return GSIncType.BEFORE;
+		}
+		return incType;
+	}
+};
+
+readonly(GSIncType,
+{
+	BEFORE: 		  0,
+	AFTER: 			  1,
+	LATER: 			  2,
+	INSERT: 		  3,
+	INSERT_AND_CACHE: 4
+});
+
+module.exports = GSIncType;
