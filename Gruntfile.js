3e91926db40a03a7ce7d5eb1ebf153f7b4f5b0d5
Harold Ritter <harold.ritter@gmail.com>
2022-09-06 20:37:09 +0200

    Adds the GruntFile for the test

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// The path module
+const path = require("path");
+
+// The collection of paths
+const PATH =
+{
+	SOURCE_DIR: "test/source",
+	DEST: "final",
+	DEST_FULL: "final/full"
+};
+PATH.SOURCE = PATH.SOURCE_DIR + "/A.js";
+
+// Module function
+module.exports = function(grunt)
+{
+	// Configures grunt-and-sniff
+	const GSTask = require("./index.js");
+	const gstask = new GSTask(grunt,
+			PATH.SOURCE_DIR,
+			{
+				verbose: 2,
+				copyDest: PATH.DEST_FULL,
+				separator: "\n", // This is indeed the default one
+				header: "/* [---o---[ [FILE] <%=path%> ]---o---] */\n\n",
+				footer: "\n\n/* [---x---[ [END FILE] <%=path%> ]---x---] */",
+				$: {testVar: "The content of the test variable"}
+			});
+	
+	// Configures grunt
+	grunt.initConfig(
+	{
+		pkg: grunt.file.readJSON("package.json"),
+
+		// Concatenates all files by reading all the file dependencies
+		concat:
+		{
+			options:
+			{
+				process: gstask.process
+			},
+			files:
+			{
+				src: PATH.SOURCE,
+				dest: path.join(PATH.DEST, "test.js")
+			}
+		}
+	});
+
+	// Loads all the grunt tasks
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	
+	// Register the default and test task (for now it is the same)
+	grunt.registerTask("default", [ "concat" ]);
+	grunt.registerTask("test", [ "concat" ]);
+};
