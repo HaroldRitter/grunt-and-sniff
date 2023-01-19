c3b0b89c4639d5f7373ceb1ad0c14fa7875d5ea3
Harold Ritter <harold.ritter@gmail.com>
2023-01-19 19:41:02 +0100

    Adds the addTask static method to GSTask

--- source/Task.class.js
+++ source/Task.class.js
@@ -12,6 +12,70 @@ const readonly = require("./utils/readonly.js");
 
 class GSTask
 {
+// ------> GSTask - Public Static Methods
+
+	/**
+	* A task function.
+	* @callback taskCallback
+	* @memberof GSTask
+	* @this Object The ``this`` keyword refers to the added task that
+	*				uses this callback. It allows to add custom functions
+	*				to a task created with {@link GSTask.addTask}.
+	* @param {GSTask} task - The gs task.
+	* @param {GSIncluder} includer
+	* @param {object} options - The options passed to the
+	*		{@link GSTask#registerTask} (or {@link GSTask#task})
+	*		method.
+	*/
+
+	/**
+	* Adds a new type of task.
+	* @param {object} task - The task data. It can also contain
+	*						other functions that are accessible
+	*						with ``this`` from the task function.
+	* 	@param {string} task.name - The name of the task in the form:
+	*						``[a-z][a-zA-Z0-9]*``.
+	*	@param {GSTask.taskCallback} [task.task] - The task function.
+	*						The ``this`` keyword refers to this added task.
+	*	@param {string} [task.description] - The description of the task
+	*						passed to grunt.
+	* @returns {Function} The constructor of GSTask.
+	*/
+	static addTask(task = {})
+	{
+		// Checks the arguments
+		const name = task.name,
+			  strName = typeof(name) == "string" ? '"' + name + '"' : name;
+		
+		GSError.openGroup(`GSTask.addTask - Checking (${strName})`);
+		
+		if( typeof(name) != "string" ||
+			!/^[a-z][a-zA-Z0-9]*$/.test(name))
+		{
+			GSError.throw(`The task name must fit "[a-z][a-zA-Z0-9]*", not ${strName}`);
+		}
+		else if(GSTask.tasks.hasOwnProperty(name))
+		{
+			GSError.throw(`The task ${name} already exists.`);
+		}
+
+		if(!task.description)
+		{
+			task.description = `The ${strName} task`;
+		}
+
+		if(typeof(task.task) != "function")
+		{
+			GSError.throw(`The task function is missing`);
+		}
+
+		GSError.closeGroup();
+
+		GSTask.tasks[name] = task;
+
+		return GSTask;
+	}
+
 // ------> GSTask - Constructor
 
 	// (grunt, sourceDir, options)
