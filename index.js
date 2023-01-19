54b1d7839ec5017ef6ffc4404ef89f3c22d2af21
Harold Ritter <harold.ritter@gmail.com>
2023-01-19 19:44:56 +0100

    Adds the tasks in the task files automatically

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 // The module only exports the task class
 var GSTask = require("./source/Task.class.js");
 
+// Adds all the tasks
+const fs = require("fs");
+fs.readdirSync("./source/tasks").forEach((file) =>
+{
+	file = "./source/tasks/" + file;
+	if(fs.statSync(file).isFile())
+	{
+		require(file);
+	}
+});
+
+
 // -----> Exports the namespace
 
 module.exports = GSTask;
